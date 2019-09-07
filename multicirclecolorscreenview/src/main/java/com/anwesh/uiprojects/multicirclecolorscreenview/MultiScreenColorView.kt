3922bc32ce4a51b4f8acadabdf97c9b39335a7e8
Anwesh43 <anweshthecool0@gmail.com>
2019-09-08 02:30:23 +0530

    adding Int and Float extension methods

--- multicirclecolorscreenview/src/main/java/com/anwesh/uiprojects/multicirclecolorscreenview/MultiScreenColorView.kt
+++ multicirclecolorscreenview/src/main/java/com/anwesh/uiprojects/multicirclecolorscreenview/MultiScreenColorView.kt
@@ -16,3 +16,7 @@ val circles : Int = 5
 val scGap : Float = 0.01f
 val colors : Array<String> = arrayOf("#311B92", "#00C853", "#f44336", "#0D47A1", "#F57F17")
 val delay : Long = 20
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n))
