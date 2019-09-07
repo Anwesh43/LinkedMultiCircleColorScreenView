2f1397ff9d51aced708e6f1a3c9b5cd3acdc4835
Anwesh43 <anweshthecool0@gmail.com>
2019-09-08 02:28:06 +0530

    adding constants and libraries

--- multicirclecolorscreenview/src/main/java/com/anwesh/uiprojects/multicirclecolorscreenview/MultiScreenColorView.kt
+++ multicirclecolorscreenview/src/main/java/com/anwesh/uiprojects/multicirclecolorscreenview/MultiScreenColorView.kt
@@ -3,3 +3,16 @@ package com.anwesh.uiprojects.multicirclecolorscreenview
 /**
  * Created by anweshmishra on 08/09/19.
  */
+
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.view.View
+import android.view.MotionEvent
+
+val circles : Int = 5
+val scGap : Float = 0.01f
+val colors : Array<String> = arrayOf("#311B92", "#00C853", "#f44336", "#0D47A1", "#F57F17")
+val delay : Long = 20
