fe8038cca2e1fe4ab4b766ec5badb6209f89f013
Anwesh43 <anweshthecool0@gmail.com>
2019-09-08 03:08:14 +0530

    using renderer in view

--- multicirclecolorscreenview/src/main/java/com/anwesh/uiprojects/multicirclecolorscreenview/MultiScreenColorView.kt
+++ multicirclecolorscreenview/src/main/java/com/anwesh/uiprojects/multicirclecolorscreenview/MultiScreenColorView.kt
@@ -58,15 +58,16 @@ fun Canvas.drawMSCNode(i : Int, scale : Float, sc : Float, currI : Int, paint : 
 class MultiScreenColorView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val renderer : Renderer = Renderer(this)
 
     override fun onDraw(canvas : Canvas) {
-
+        renderer.render(canvas, paint)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
