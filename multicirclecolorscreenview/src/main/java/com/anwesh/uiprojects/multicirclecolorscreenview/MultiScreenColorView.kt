19bd63eabd79dbf5985cdc01a0e17f8198bf9ca2
Anwesh43 <anweshthecool0@gmail.com>
2019-09-08 02:45:37 +0530

    adding MultiScreenColorView

--- multicirclecolorscreenview/src/main/java/com/anwesh/uiprojects/multicirclecolorscreenview/MultiScreenColorView.kt
+++ multicirclecolorscreenview/src/main/java/com/anwesh/uiprojects/multicirclecolorscreenview/MultiScreenColorView.kt
@@ -53,3 +53,21 @@ fun Canvas.drawMSCNode(i : Int, scale : Float, sc : Float, currI : Int, paint : 
     drawMultiCircleScreen(i, size, scale, sc, i == currI, paint)
     restore()
 }
+
+class MultiScreenColorView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
